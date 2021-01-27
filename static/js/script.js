08912d334d112c334e562e46c733a8f7c12a33f8
narrensingh <narren.singh.code@gmail.com>
2021-01-27 21:56:25 +0530

    challenge2:cat generator

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,29 @@
 // Challenge 1 : Age in days
 
 function ageIndays() {
-  var dob = prompt("What is the year you were born");
+  var dob = prompt('What is the year you were born');
   var age_days = (2021 - dob) * 365;
-  var h1 = document.createElement("h1");
+  var h1 = document.createElement('h1');
   var textAnswer = document.createTextNode(
-    "Your are " + age_days + " days old"
+    'Your are ' + age_days + ' days old'
   );
-  h1.setAttribute("id", "ageInDays");
+  h1.setAttribute('id', 'ageInDays');
   h1.appendChild(textAnswer);
-  document.getElementById("flex-box-result").appendChild(h1);
+  document.getElementById('flex-box-container-result').appendChild(h1);
 }
 
 function reset() {
-  document.getElementById("ageInDays").remove();
+  document.getElementById('ageInDays').remove();
+}
+
+function generatecat() {
+  var Image = document.createElement('img');
+  var div = document.getElementById('catgenimg');
+  Image.setAttribute('id', 'catimg');
+  Image.src = 'https://media4.giphy.com/media/jRlP4zbERYW5HoCLvX/200_d.gif';
+  div.appendChild(Image);
+}
+
+function resetcontainer2() {
+  document.getElementById('catimg').remove();
 }
